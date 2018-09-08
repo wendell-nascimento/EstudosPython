5561166beaedc709f6591970aebf20dd5761ffed
Nascimento <nascimento.com.br@gmail.com>
2018-09-08 17:31:11 -0400

    Estudos

--- cadastrar.py
+++ cadastrar.py
@@ -1,4 +1,21 @@
 nomes=[]
+
+def menu():
+	print '\n 1 - Cadastrar \n 2 - Remover \n 3 - Listar \n 0 - Sair \n'
+	print 'Digite sua escolha: '
+	op=raw_input()
+	
+	
+	if(op=='1'):
+		cadastrar()
+		menu()
+	if(op=='2'):
+		remover()
+		menu()
+	if(op=='3'):
+		listar()
+		menu()
+
 def cadastrar():
 	print 'Digite um nome: '
 	nome=raw_input()
@@ -11,3 +28,9 @@ def remover():
 	nomes.remove(nome)
 	print 'Resultado: '
 	print nomes
+
+def listar():
+	print 'Usuarios cadastrados ate o momento: '
+	print nomes
+
+menu()
