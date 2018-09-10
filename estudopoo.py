350f476d899e3baaf8ac2903b90caa42c4eb50d0
Nascimento <nascimento.com.br@gmail.com>
2018-09-09 20:22:41 -0400

    atualizacao

--- estudopoo.py
+++ estudopoo.py
@@ -7,6 +7,13 @@ class Perfil(object):
 		self.nome=nome
 		self.telefone=telefone
 		self.empresa=empresa
+		self.__curtidas=0
 	
 	def imprimir(self):
 		print 'Nome: %s, Telefone: %s, Empresa: %s' % (self.nome, self.telefone, self.empresa)
+	
+	def curtir(self):
+		self.__curtidas+=1
+	
+	def obter_curtidas(self):
+		return self.__curtidas
